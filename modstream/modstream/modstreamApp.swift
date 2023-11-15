1d08b74d16cfdc49a61915529c3aa3b985216c83
JB184351 <justinbengtson@icloud.com>
2023-11-15 13:15:08 -0800

    Created ModStream project!

---
+++ modstream/modstream/modstreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  modstreamApp.swift
+//  modstream
+//
+//  Created by Justin on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct modstreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
